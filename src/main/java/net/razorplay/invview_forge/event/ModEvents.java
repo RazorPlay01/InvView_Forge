ffd418a02e3f0c8eefdc1c8781483d9f2704eb6f
RazorPlay01 <82621554+RazorPlay01@users.noreply.github.com>
2024-04-04 12:01:37 -0400

    Fix bug preventing item duplication when a player opened a disconnected player's container, and target player connected while the container was still open

--- src/main/java/net/razorplay/invview_forge/event/ModEvents.java
+++ src/main/java/net/razorplay/invview_forge/event/ModEvents.java
@@ -1,11 +1,17 @@
 package net.razorplay.invview_forge.event;
 
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.razorplay.invview_forge.InvView_Forge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.server.command.ConfigCommand;
 import net.razorplay.invview_forge.command.InvViewCommands;
+import net.razorplay.invview_forge.container.PlayerEnderChestScreenHandler;
+import net.razorplay.invview_forge.container.PlayerInventoryScreenHandler;
+
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid = InvView_Forge.MOD_ID)
 public class ModEvents {
@@ -16,4 +22,27 @@ public class ModEvents {
 
         ConfigCommand.register(event.getDispatcher());
     }
+
+    @SubscribeEvent
+    public static void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
+        if (!isPlayerUnique(PlayerInventoryScreenHandler.invScreenTargetPlayers, (ServerPlayer) event.getEntity()) ||
+                !isPlayerUnique(PlayerEnderChestScreenHandler.endChestScreenTargetPlayers, (ServerPlayer) event.getEntity())) {
+
+            List<ServerPlayer> serverPlayers = event.getEntity().getServer().getPlayerList().getPlayers();
+
+            serverPlayers.forEach(player -> {
+                if (player.containerMenu instanceof PlayerEnderChestScreenHandler ||
+                        player.containerMenu instanceof PlayerInventoryScreenHandler) {
+                    player.closeContainer();
+                }
+            });
+        }
+    }
+
+    private static boolean isPlayerUnique(List<ServerPlayer> players, ServerPlayer targetPlayer) {
+        return players.stream()
+                .noneMatch(player -> player.getDisplayName().equals(targetPlayer.getDisplayName()));
+    }
+
+
 }
