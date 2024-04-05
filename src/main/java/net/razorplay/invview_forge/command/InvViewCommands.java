09693b86b95670dc3e473be0138c2ff4b4e26a94
RazorPlay01 <82621554+RazorPlay01@users.noreply.github.com>
2024-04-05 11:47:52 -0400

    Initial commit

---
+++ src/main/java/net/razorplay/invview_forge/command/InvViewCommands.java
@@ -0,0 +1,134 @@
+package net.razorplay.invview_forge.command;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.serialization.Dynamic;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.GameProfileArgument;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ClientInformation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.razorplay.invview_forge.container.PlayerEnderChestScreenHandler;
+import net.razorplay.invview_forge.container.PlayerInventoryScreenHandler;
+
+import javax.annotation.Nullable;
+
+public class InvViewCommands {
+    public InvViewCommands(CommandDispatcher<CommandSourceStack> dispatcher) {
+        dispatcher.register(Commands.literal("view").requires((player) -> {
+                            return player.hasPermission(2);
+                        })
+                        .then(Commands.literal("inv")
+                                .then(Commands.argument("target", GameProfileArgument.gameProfile())
+                                        .executes(context -> executeInventoryCheck(context, (ServerPlayer) context.getSource().getEntity()))))
+                        .then(Commands.literal("echest")
+                                .then(Commands.argument("target", GameProfileArgument.gameProfile())
+                                        .executes(context -> executeEnderChestCheck(context, (ServerPlayer) context.getSource().getEntity()))))
+        );
+    }
+
+    private int executeEnderChestCheck(CommandContext<CommandSourceStack> context, ServerPlayer player) throws CommandSyntaxException {
+        ServerPlayer targetPlayer = getRequestedPlayer(context);
+        boolean canOpen = true;
+
+        if (!PlayerEnderChestScreenHandler.endChestScreenTargetPlayers.isEmpty()) {
+            for (int i = 0; i < PlayerEnderChestScreenHandler.endChestScreenTargetPlayers.size(); i++) {
+                if (PlayerEnderChestScreenHandler.endChestScreenTargetPlayers.get(i).getDisplayName().equals(targetPlayer.getDisplayName())) {
+                    canOpen = false;
+                    break;
+                }
+            }
+        }
+        if (canOpen) {
+            MenuProvider screenHandlerFactory = new MenuProvider() {
+                @Override
+                public Component getDisplayName() {
+                    return targetPlayer.getDisplayName();
+                }
+
+                @Nullable
+                @Override
+                public AbstractContainerMenu createMenu(int i, Inventory inventory, Player player_) {
+                    return new PlayerEnderChestScreenHandler(i, player, targetPlayer);
+
+                }
+            };
+
+
+            player.openMenu(screenHandlerFactory);
+        } else {
+            context.getSource().sendFailure(Component.literal("ERROR: The enderchest container is already being used by another player."));
+        }
+        return 1;
+    }
+
+    private int executeInventoryCheck(CommandContext<CommandSourceStack> context, ServerPlayer player) throws CommandSyntaxException {
+        ServerPlayer targetPlayer = getRequestedPlayer(context);
+
+        boolean canOpen = true;
+
+        if (!PlayerInventoryScreenHandler.invScreenTargetPlayers.isEmpty()) {
+            for (int i = 0; i < PlayerInventoryScreenHandler.invScreenTargetPlayers.size(); i++) {
+                if (PlayerInventoryScreenHandler.invScreenTargetPlayers.get(i).getDisplayName().equals(targetPlayer.getDisplayName())) {
+                    canOpen = false;
+                    break;
+                }
+            }
+        }
+        if (canOpen) {
+            MenuProvider screenHandlerFactory = new MenuProvider() {
+                @Override
+                public Component getDisplayName() {
+                    return targetPlayer.getDisplayName();
+                }
+
+                @Nullable
+                @Override
+                public AbstractContainerMenu createMenu(int i, Inventory inventory, Player player_) {
+                    return new PlayerInventoryScreenHandler(i, player, targetPlayer);
+
+                }
+            };
+
+            player.openMenu(screenHandlerFactory);
+        } else {
+            context.getSource().sendFailure(Component.literal("ERROR: The inventory container is already being used by another player."));
+        }
+        return 1;
+    }
+
+    private static ServerPlayer getRequestedPlayer(CommandContext<CommandSourceStack> context)
+            throws CommandSyntaxException {
+        MinecraftServer minecraftServer = context.getSource().getServer();
+
+        GameProfile requestedProfile = GameProfileArgument.getGameProfiles(context, "target").iterator().next();
+        ServerPlayer requestedPlayer = minecraftServer.getPlayerList().getPlayerByName(requestedProfile.getName());
+
+        if (requestedPlayer == null) {
+            requestedPlayer = minecraftServer.getPlayerList().getPlayerForLogin(requestedProfile, ClientInformation.createDefault());
+            CompoundTag compound = minecraftServer.getPlayerList().load(requestedPlayer);
+            if (compound != null) {
+                ServerLevel world = minecraftServer.getLevel(
+                        DimensionType.parseLegacy(new Dynamic<>(NbtOps.INSTANCE, compound.get("Dimension")))
+                                .result().get());
+
+                if (world != null) {
+                    requestedPlayer.setServerLevel(world);
+                }
+            }
+        }
+        return requestedPlayer;
+    }
+}
---
+++ src/main/java/net/razorplay/invview_forge/event/ModEvents.java
@@ -0,0 +1,48 @@
+package net.razorplay.invview_forge.event;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.server.command.ConfigCommand;
+import net.razorplay.invview_forge.InvView_Forge;
+import net.razorplay.invview_forge.command.InvViewCommands;
+import net.razorplay.invview_forge.container.PlayerEnderChestScreenHandler;
+import net.razorplay.invview_forge.container.PlayerInventoryScreenHandler;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = InvView_Forge.MOD_ID)
+public class ModEvents {
+
+    @SubscribeEvent
+    public static void onCommandsRegister(RegisterCommandsEvent event) {
+        new InvViewCommands(event.getDispatcher());
+
+        ConfigCommand.register(event.getDispatcher());
+    }
+
+    @SubscribeEvent
+    public static void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
+        if (!isPlayerUnique(PlayerInventoryScreenHandler.invScreenTargetPlayers, (ServerPlayer) event.getEntity()) ||
+                !isPlayerUnique(PlayerEnderChestScreenHandler.endChestScreenTargetPlayers, (ServerPlayer) event.getEntity())) {
+
+            List<ServerPlayer> serverPlayers = event.getEntity().getServer().getPlayerList().getPlayers();
+
+            serverPlayers.forEach(player -> {
+                if (player.containerMenu instanceof PlayerEnderChestScreenHandler ||
+                        player.containerMenu instanceof PlayerInventoryScreenHandler) {
+                    player.closeContainer();
+                }
+            });
+        }
+    }
+
+    private static boolean isPlayerUnique(List<ServerPlayer> players, ServerPlayer targetPlayer) {
+        return players.stream()
+                .noneMatch(player -> player.getDisplayName().equals(targetPlayer.getDisplayName()));
+    }
+
+
+}
